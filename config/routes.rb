985ea31b559be342333a57908d78fe4fd6e545b5
Artem Vakhutinskiy <vahut@yandex.ru>
2024-03-22 00:37:44 +0000

    student dashboard view and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Example route for adding a new task. Adjust according to your actual controller and action names.
   get 'teachers/add_new_task', to: 'teachers#add_new_task', as: 'new_add_task'
 
+  get 'students/index', to: 'students#index'
+
   # Defines the root path route ("/")
   root "pages#home"
 end

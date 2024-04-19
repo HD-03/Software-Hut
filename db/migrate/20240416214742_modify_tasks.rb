37bc0e80767fa0467456becb8aac8e0c2ffbd3ef
Artis Levensteins <alevensteins1@sheffield.ac.uk>
2024-04-19 16:20:26 +0100

    Fixed some migrations issues

--- db/migrate/20240416214742_modify_tasks.rb
+++ db/migrate/20240416214742_modify_tasks.rb
@@ -1,6 +1,6 @@
 class ModifyTasks < ActiveRecord::Migration[7.0]
   def change
-    rename_column :tasks, :teacher, :teacher_id
-    rename_column :tasks, :student, :student_id
+    rename_column :tasks, :teacher_user_id, :teacher_id
+    rename_column :tasks, :student_user_id, :student_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_16_225207) do
     t.text "description"
     t.datetime "time_set", null: false
     t.datetime "deadline", null: false
-    t.integer "reward_xp", null: false
+    t.integer "base_experience_points", null: false
     t.integer "status", default: 0, null: false
     t.string "attachment_paths", default: [], array: true
+    t.boolean "recording_boolean", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "recording_paths"
   end
 
   create_table "users", force: :cascade do |t|
@@ -75,6 +75,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_16_225207) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "tasks", "users", column: "student_id"
-  add_foreign_key "tasks", "users", column: "teacher_id"
 end

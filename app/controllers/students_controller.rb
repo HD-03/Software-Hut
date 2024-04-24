4e5113a6f56ceb2745efe8a5fdd53c2a74cf86f5
Art1ns <artislev2@gmail.com>
2024-04-24 14:55:15 +0100

    got rid of unneeded commented out code from controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,26 +15,4 @@ class StudentsController < ApplicationController
     @show_level_up_modal = current_user.recently_leveled_up?
   end
 
-  # if current_user.recently_leveled_up
-  #   puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA RUNNING MODAL"
-
-  #   :javascript
-  #     $('#levelUpModal').modal('show');
-  #   end
-
-  #   current_user.update(recently_leveled_up: false)
-  # end
-
-  # def display_level_up_modal
-  #   if current_user.recently_leveled_up == true
-  #     puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA RUNNING MODAL"
-  #     respond_to do |format|
-  #       format.js { render partial: 'students/level_up_modal' }
-  #     end
-
-  #     current_user.update(recently_leveled_up: false)
-  #     puts "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
-  #   end
-  # end
-
 end

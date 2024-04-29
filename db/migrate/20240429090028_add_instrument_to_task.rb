39bc8007b6e66f605819ab8f4f7188984fadaf0b
Markus Teimann <mteimann1@sheffield.ac.uk>
2024-04-29 11:27:36 +0100

    deployment is failing due to some migration issue

--- db/migrate/20240429090028_add_instrument_to_task.rb
+++ db/migrate/20240429090028_add_instrument_to_task.rb
@@ -1,5 +1,5 @@
-class AddInstrumentToTask < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :tasks, :instrument, null: false, foreign_key: true, default: 1
-  end
-end
+# class AddInstrumentToTask < ActiveRecord::Migration[7.0]
+#   def change
+#     add_reference :tasks, :instrument, null: false, foreign_key: true, default: 1
+#   end
+# end

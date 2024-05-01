383cc59c3814a7b1aa5bc53ae3973a060ce094ab
Artem Vakhutinskiy <vahut@yandex.ru>
2024-05-01 15:24:02 +0100

    active storage done, added file input with validation to add, edit user and add task, cosmetic changes to admin dashboard

--- app/models/task.rb
+++ app/models/task.rb
@@ -31,6 +31,8 @@ class Task < ApplicationRecord
   belongs_to :student, -> { where(role: 0) }, class_name: 'User'
   belongs_to :instrument
 
+  has_many_attached :files
+
   enum status: { todo: 0, pending: 1, completed: 2 }
 
   # Returns the day of the week for the deadline if it falls within the current week,

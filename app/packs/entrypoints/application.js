d62698daa2383530da83076ead0deb004cd9a32b
OneLastWish <JTyrrell1@sheffield.ac.uk>
2024-05-10 13:29:19 +0100

    moved files around

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs";
 import 'jquery'
 import 'popper.js'
 import "bootstrap";
+import '../scripts/voice_controller.js'
 // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 // window.bootstrap = require("bootstrap")
 

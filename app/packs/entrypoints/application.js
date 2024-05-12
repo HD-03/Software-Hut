6834d1cf5a8983e39d1d759474b81da00d7d9046
Artem Vakhutinskiy <vahut@yandex.ru>
2024-05-12 18:27:59 +0100

    merge with main

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -2,9 +2,20 @@ import Rails from "@rails/ujs";
 import 'jquery'
 import 'popper.js'
 import "bootstrap";
-import '../scripts/voice_controller.js'
-import "controllers"
-import '@hotwired/stimulus'
+
+import { Application, defaultSchema } from 'stimulus'
+import { definitionsFromContext } from "stimulus-webpack-helpers"
+
+const customSchema = {
+    ...defaultSchema,
+    controllerAttribute: 'data-stimulus-controller',
+    actionAttribute: 'data-stimulus-action',
+    targetAttribute: 'data-stimulus-target'
+}
+
+window.Stimulus = Application.start(document.documentElement, customSchema)
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
 
 // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 // window.bootstrap = require("bootstrap")

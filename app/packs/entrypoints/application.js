f2f8006eab66a23ee3f0e31af59607f43488a986
Jtyrrell <Jtyrrell1@sheffield.ac.uk>
2024-05-12 15:54:09 +0100

    minor changes

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -3,6 +3,7 @@ import 'jquery'
 import 'popper.js'
 import "bootstrap";
 import '../scripts/voice_controller.js'
+
 // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 // window.bootstrap = require("bootstrap")
 

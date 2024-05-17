291be164cb6b805452c74766051e676cf307892e
aca21zk <zkhan18@sheffield.ac.uk>
2024-05-17 16:14:10 +0100

    commiting changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema[7.0].define(version: 2024_04_29_152314) do
 ActiveRecord::Schema[7.0].define(version: 2024_04_29_152445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +86,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_29_152445) do
     t.boolean "recording_boolean", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "instrument_id", default: 1, null: false
+    t.index ["instrument_id"], name: "index_tasks_on_instrument_id"
   end
 
   create_table "users", force: :cascade do |t|
